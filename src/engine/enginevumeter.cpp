2fa36f6f1f6a505dfa837a580162277b68935499
Alexander Graf <graf@chiptoverse.com>
2022-12-29 07:06:02 +0100

    EngineVuMeter: remove redundant args in ControlPotmeter initializations
    The given arguments are the default arguments of the ControlPotmeter ctor;
    they can be omitted.

--- src/engine/enginevumeter.cpp
+++ src/engine/enginevumeter.cpp
@@ -20,20 +20,17 @@ constexpr CSAMPLE kDecaySmoothing = 0.1f;  //.16//.4
 EngineVuMeter::EngineVuMeter(const QString& group):
     // The VUmeter widget is controlled via a controlpotmeter, which means
     // that it should react on the setValue(int) signal.
-    m_ctrlVuMeter(ConfigKey(group, "VuMeter"), 0., 1.),
+    m_ctrlVuMeter(ConfigKey(group, "VuMeter")),
     // left channel VU meter
-    m_ctrlVuMeterL(ConfigKey(group, "VuMeterL"), 0., 1.),
+    m_ctrlVuMeterL(ConfigKey(group, "VuMeterL")),
     // right channel VU meter
-    m_ctrlVuMeterR(ConfigKey(group, "VuMeterR"), 0., 1.),
+    m_ctrlVuMeterR(ConfigKey(group, "VuMeterR")),
 
     // Used controlpotmeter as the example used it :/ perhaps someone with more
     // knowledge could use something more suitable...
-    m_ctrlPeakIndicator(ConfigKey(group, "PeakIndicator"),
-                                              0., 1.),
-    m_ctrlPeakIndicatorL(ConfigKey(group, "PeakIndicatorL"),
-                                              0., 1.),
-    m_ctrlPeakIndicatorR(ConfigKey(group, "PeakIndicatorR"),
-                                              0., 1.),
+    m_ctrlPeakIndicator(ConfigKey(group, "PeakIndicator")),
+    m_ctrlPeakIndicatorL(ConfigKey(group, "PeakIndicatorL")),
+    m_ctrlPeakIndicatorR(ConfigKey(group, "PeakIndicatorR")),
     m_sampleRate("[Master]", "samplerate") {
     // Initialize the calculation:
     reset();
